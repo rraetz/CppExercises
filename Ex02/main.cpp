ccb6c1d5a7b76be8d3cd789e17febe3edc7a912b
raphaelMLN <raphael.raetz@master.hes-so.ch>
2020-10-03 00:47:21 +0200

    Ex02 solved w/o voluntary exercises

--- Ex02/main.cpp
+++ Ex02/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <map>
 #include <string>
 #include <iterator>
 #include <list>
@@ -17,35 +18,27 @@ using namespace std;
 class Billionaire
 {
 private:
+
+
+public:
     string m_name;
     string m_salary;
     string m_country;
-
-public:
     friend istream &operator >> (istream &is, Billionaire  &billionaire) 
     {
-        string tmp;
-        getline(is, tmp);
-        if (!tmp.empty())
+        if (is.peek() != is.eof())
         {
-            // Find start & end of name, salary and country
-            auto startSalary = tmp.find_first_of("0123456789");
-            auto endName = startSalary - 1;
-            auto endSalary = tmp.find_last_of("0123456789");
-            auto startCountry = tmp.find_last_of("\t") + 1;
-
-            // Store in member variables
-            billionaire.m_name = tmp.substr(0, endName);
-            billionaire.m_salary = tmp.substr(startSalary, endSalary-startSalary+1);
-            billionaire.m_country = tmp.substr(startCountry, tmp.length()-startCountry+1);
+            getline(is, billionaire.m_name, '\t');
+            getline(is, billionaire.m_salary, '\t');
+            getline(is, billionaire.m_country, '\n');
         }
         return is;
     }
 
     friend ostream &operator << (ostream &os, const Billionaire  &billionaire) 
     {
-        os << "Name: " << billionaire.m_name << "\nSalary: " << billionaire.m_salary 
-            << "\nCountry:" << billionaire.m_country;
+        os << "Name: " << billionaire.m_name << " Salary: " << billionaire.m_salary 
+            << " Country:" << billionaire.m_country;
         return os;
     }
 };
@@ -56,49 +49,49 @@ int main()
 {
     
 // PHONE BOOK
-    // // Data as given by the exercise
-    // vector<pair<string, string>> vec{ 
-    //     make_pair ("Homer", "1111"),
-    //     make_pair ("Marge", "2222"),
-    //     make_pair ("Lisa", "3333"),
-    //     make_pair ("Magy", "4444"),
-    //     make_pair ("Bart", "5555")};
+    // Data as given by the exercise
+    vector<pair<string, string>> vec{ 
+        make_pair ("Homer", "1111"),
+        make_pair ("Marge", "2222"),
+        make_pair ("Lisa", "3333"),
+        make_pair ("Magy", "4444"),
+        make_pair ("Bart", "5555")};
 
-    // // Create phoneBook and copy content of vec
-    // unordered_multimap<string, string> phoneBook;
-    // copy(vec.begin(), vec.end(), inserter(phoneBook, phoneBook.end()));
+    // Create phoneBook and copy content of vec
+    unordered_multimap<string, string> phoneBook;
+    copy(vec.begin(), vec.end(), inserter(phoneBook, phoneBook.end()));
 
-    // // Add additional number for Homer
-    // phoneBook.emplace(make_pair("Homer", "1234"));
+    // Add additional number for Homer
+    phoneBook.emplace(make_pair("Homer", "1234"));
     
 
-    // // Get phone numbers
-    // string inputName;
-    // while(1)
-    // {   
-    //     cout << "Enter name (or Exit): ";
-    //     cin >> inputName;
+    // Get phone numbers
+    string inputName;
+    while(1)
+    {   
+        cout << "Enter name (or Exit): ";
+        cin >> inputName;
 
-    //     // Break if "exit" or "Exit"
-    //     if (inputName.compare("Exit") == 0 || inputName.compare("exit") == 0) 
-    //     {
-    //         break;
-    //     }
+        // Break if "exit" or "Exit"
+        if (inputName.compare("Exit") == 0 || inputName.compare("exit") == 0) 
+        {
+            break;
+        }
 
-    //     // Find name in phone book and print if available
-    //     if (phoneBook.find(inputName) == phoneBook.end())
-    //     {
-    //         cout << "Name not in directory" << endl;
-    //     }
-    //     else
-    //     {
-    //         auto its = phoneBook.equal_range(inputName);
-    //         for (auto it = its.first; it != its.second; it++)
-    //         {
-    //             cout << "Phone number: " << it->second << endl;
-    //         }
-    //     }   
-    // }
+        // Find name in phone book and print if available
+        if (phoneBook.find(inputName) == phoneBook.end())
+        {
+            cout << "Name not in directory" << endl;
+        }
+        else
+        {
+            auto its = phoneBook.equal_range(inputName);
+            for (auto it = its.first; it != its.second; it++)
+            {
+                cout << "Phone number: " << it->second << endl;
+            }
+        }   
+    }
 
 
 
@@ -115,7 +108,31 @@ int main()
    
     vector<Billionaire> billionaires;
     copy(istream_iterator<Billionaire>(stream), istream_iterator<Billionaire>(), back_inserter(billionaires));
+    cout << "\n List of billionaires:" << endl;
     copy(billionaires.begin(), billionaires.end(), ostream_iterator<Billionaire>(cout , "\n"));
+
+
+    // Copy of first billionaire, counter with # of billionaires per country>
+    map<string, pair<const Billionaire, size_t>> billiMap;
+    for (const auto &e:billionaires)
+    {
+        // Check if key for e.m_country already exists, if yes, increment counter
+        auto check = billiMap.try_emplace(e.m_country, e, 1);
+        if (!check.second)
+        {
+            // dereference iterator, access pair, access counter
+            check.first->second.second++; 
+        }
+    }
+
+    // Print the map
+    cout << "\n Billionaires per country:" << endl;
+    for (const auto &e:billiMap)
+    {
+        cout << e.first <<": " << e.second.second << 
+        " billionaires.  Richest is " << e.second.first.m_name << 
+        " with " << e.second.first.m_salary << "$" << endl;
+    }
 
 
     return 0;
